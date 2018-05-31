9786c3b8da4caf8892bf266137a6502077b89c0a
sivakk <ksiva1355@gail.com>
2018-05-31 09:29:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="container text-center">
-        <h1>Simple Noteee</h1>
+        <h1>Simple Noteeeeeee</h1>
       </div>
     );
   }
